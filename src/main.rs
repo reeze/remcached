568def43c865757e0aff61eb0d0a82931e863941
Reeze Xia <reeze@php.net>
2016-09-11 15:16:23 +0800

    explict set log level to debug for now

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,13 @@ use std::env;
 use std::process::exit;
 use std::net::SocketAddr;
 
-use remcached::logger;
+use remcached::logger::RemcachedLogger;
+
+use log::LogLevel;
 
 fn main()
 {
+    RemcachedLogger::new(LogLevel::Debug).init().unwrap();
 
     let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
     let addr = match addr.parse::<SocketAddr>() {
